f2e7aa577ee1fa722e1589b0e3c076b275d7a582
kkarczewski <serwisyrejestracje@wp.pl>
2014-10-13 22:53:17 +0200

    62-62
    skróty w Eclipsie i debugowanie

---
+++ 62 - Skróty klawiaturowe/src/App.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+		System.err.println("ERROR");
+		// List list;
+		ArrayList another;
+		another = new ArrayList<>();
+	}
+}
+// ALT+SHIFT+R - Zmiana nazwy wielu naraz
+// 
---
+++ 63 - Debbugging/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+	public static void main(String[] args) {
+		int a = 7;
+
+		System.out.println("Hello");
+
+		System.out.println("Starting");
+		a++;
+		a = a - 8;
+		System.out.println("Finishing");
+
+	}
+}
